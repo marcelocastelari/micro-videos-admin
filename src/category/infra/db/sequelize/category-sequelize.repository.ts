4f3905b81126ff0942cec6ea7c307db9f36ecf56
marcelocastelari <marcelocastelari@gmail.com>
2024-01-25 15:41:11 +0000

    feat: create category sequelize repository (update, delete and search)

--- src/category/infra/db/sequelize/category-sequelize.repository.ts
+++ src/category/infra/db/sequelize/category-sequelize.repository.ts
@@ -1,10 +1,12 @@
-import { Entity } from "../../../../shared/domain/entity";
+import { Op } from "sequelize";
 import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
-import { SearchParams } from "../../../../shared/domain/repository/search-params";
-import { SearchResult } from "../../../../shared/domain/repository/search-result";
 import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../../../domain/category.entity";
-import { ICategoryRepository } from "../../../domain/category.repository";
+import {
+    CategorySearchParams,
+    CategorySearchResult,
+    ICategoryRepository,
+} from "../../../domain/category.repository";
 import { CategoryModel } from "./category.model";
 
 export class CategorySequelizeRepository implements ICategoryRepository {
@@ -51,6 +53,7 @@ export class CategorySequelizeRepository implements ICategoryRepository {
             { where: { category_id: id } }
         );
     }
+
     async delete(category_id: Uuid): Promise<void> {
         const id = category_id.id;
         const model = await this._get(id);
@@ -88,8 +91,35 @@ export class CategorySequelizeRepository implements ICategoryRepository {
         });
     }
 
-    search(props: SearchParams<string>): Promise<SearchResult<Entity>> {
-        throw new Error("Method not implemented.");
+    async search(props: CategorySearchParams): Promise<CategorySearchResult> {
+        const offset = (props.page - 1) * props.per_page;
+        const limit = props.per_page;
+        const { rows: models, count } = await this.categoryModel.findAndCountAll({
+            ...(props.filter && {
+                where: {
+                    name: { [Op.like]: `%${props.filter}%` },
+                },
+            }),
+            ...(props.sort && this.sortableFields.includes(props.sort)
+                ? { order: [[props.sort, props.sort_dir]] }
+                : { order: [["created_at", "desc"]] }),
+            offset,
+            limit,
+        });
+        return new CategorySearchResult({
+            items: models.map((model) => {
+                return new Category({
+                    category_id: new Uuid(model.category_id),
+                    name: model.name,
+                    description: model.description,
+                    is_active: model.is_active,
+                    created_at: model.created_at,
+                });
+            }),
+            current_page: props.page,
+            per_page: props.per_page,
+            total: count,
+        })
     }
 
     getEntity(): new (...args: any[]) => Category {
