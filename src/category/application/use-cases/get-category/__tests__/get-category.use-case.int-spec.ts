ddc8d21aef4a1887102834341d26ff8c185dfff9
marcelocastelari <marcelocastelari@gmail.com>
2024-02-16 14:25:17 +0000

    feat: create use case inputs validations

--- src/category/application/use-cases/get-category/__tests__/get-category.use-case.int-spec.ts
+++ src/category/application/use-cases/get-category/__tests__/get-category.use-case.int-spec.ts
@@ -4,17 +4,17 @@ import { setupSequelize } from "../../../../../shared/infra/testing/helpers";
 import { Category } from "../../../../domain/category.entity";
 import { CategorySequelizeRepository } from "../../../../infra/db/sequelize/category-sequelize.repository";
 import { CategoryModel } from "../../../../infra/db/sequelize/category.model";
-import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+import { GetCategoryUseCase } from "../get-category.use-case";
 
-describe("DeleteCategoryUseCase Integration Tests", () => {
-    let useCase: DeleteCategoryUseCase;
+describe("GetCategoryUseCase Integration Tests", () => {
+    let useCase: GetCategoryUseCase;
     let repository: CategorySequelizeRepository;
 
     setupSequelize({ models: [CategoryModel] });
 
     beforeEach(() => {
         repository = new CategorySequelizeRepository(CategoryModel);
-        useCase = new DeleteCategoryUseCase(repository);
+        useCase = new GetCategoryUseCase(repository);
     });
 
     it("should throws error when entity not found", async () => {
@@ -24,12 +24,16 @@ describe("DeleteCategoryUseCase Integration Tests", () => {
         );
     });
 
-    it("should delete a category", async () => {
+    it("should returns a category", async () => {
         const category = Category.fake().aCategory().build();
         await repository.insert(category);
-        await useCase.execute({
+        const output = await useCase.execute({ id: category.category_id.id });
+        expect(output).toStrictEqual({
             id: category.category_id.id,
+            name: category.name,
+            description: category.description,
+            is_active: category.is_active,
+            created_at: category.created_at,
         });
-        await expect(repository.findById(category.category_id)).resolves.toBeNull();
     });
 });
